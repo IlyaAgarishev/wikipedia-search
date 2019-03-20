4d65c2fc96b904755ea22b38bff29f99ea46b81c
IlyaAgarishev <ilyagarishev@gmail.com>
2019-03-21 02:30:31 +0500

    only one test file - index.test.js

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,3 +1,9 @@
+import React from "react";
+import { mount, render, shallow } from "enzyme";
+import App from "../components/App";
+import Item from "../components/Item";
+import Toggle from "../components/Toggle";
+import Form from "../components/Form";
 import randomWords from "random-words";
 import {
   beautifyResponseText,
@@ -5,6 +11,8 @@ import {
   getRequestsStringPreview,
   addRequest
 } from "../requestFunctions.js";
+
+// Functions testing
 
 test("beautifyResponseText returns an array and 1st element length of this array equals 3", () => {
   const data = ["Test_0", ["Test_1"], ["Test_2"], ["www.test.com"]];
@@ -47,4 +55,59 @@ test("addRequest returns array and sum of lengths array elements <= 60 ", () => 
     expect(Array.isArray(data)).toBe(true);
     expect(data.join("").length < 60).toBe(true);
   });
+});
+
+// Components testing
+
+test("shallow App snapshot", () => {
+  const component = shallow(<App />);
+  expect(component).toMatchSnapshot();
+});
+
+test("Item snapshot darkFalse", () => {
+  const component = shallow(
+    <Item title="title" snippet="snippet" link="link" darkTheme={false} />
+  );
+  expect(component).toMatchSnapshot();
+});
+
+test("Item snapshot darkTrue", () => {
+  const component = shallow(
+    <Item title="title" snippet="snippet" link="link" darkTheme={true} />
+  );
+  expect(component).toMatchSnapshot();
+});
+
+test("Toggle snapshot", () => {
+  const component = mount(<Toggle switchTheme={jest.fn()} />);
+  expect(component).toMatchSnapshot();
+  component.unmount();
+});
+
+test("Form snapshot darkFalse", () => {
+  const component = shallow(
+    <Form
+      setDataState={jest.fn()}
+      darkTheme={false}
+      value="value"
+      setValueState={jest.fn()}
+      requests={["one", "two"]}
+      ajaxErrorState={jest.fn()}
+    />
+  );
+  expect(component).toMatchSnapshot();
+});
+
+test("Form snapshot darkTrue", () => {
+  const component = shallow(
+    <Form
+      setDataState={jest.fn()}
+      darkTheme={true}
+      value="value"
+      setValueState={jest.fn()}
+      requests={["one", "two"]}
+      ajaxErrorState={jest.fn()}
+    />
+  );
+  expect(component).toMatchSnapshot();
 });
